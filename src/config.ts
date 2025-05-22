bcada6069522ef5f4fa832f101d4f64f55301694
Bloom Perez <bloomdevelop@proton.me>
2025-05-22 01:25:01 +0000

    chore: remove unused imports

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,12 @@
-import {parse} from "valibot";
-import {ConfigSchema} from "./types";
+import { parse } from "valibot";
+import { ConfigSchema } from "./types";
 
 /**
  * @description Bot's Configuration
  */
 export const config = parse(ConfigSchema, {
-  token: process.env.TOKEN,
-  prefix: process.env.PREFIX,
-  embedColor: process.env.EMBED_COLOR || "#C29359",
-  geminiApiKey: process.env.GEMINI_API_KEY,
+	token: process.env.TOKEN,
+	prefix: process.env.PREFIX,
+	embedColor: process.env.EMBED_COLOR || "#C29359",
+	geminiApiKey: process.env.GEMINI_API_KEY,
 });
