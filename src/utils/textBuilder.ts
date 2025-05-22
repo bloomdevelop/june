79968ec71d4ef5ea35192ede07f16dc1ae12466a
Bloom Perez <bloomdevelop@proton.me>
2025-05-22 01:37:19 +0000

    chore: add JSDoc

--- src/utils/textBuilder.ts
+++ src/utils/textBuilder.ts
@@ -1,3 +1,9 @@
+/**
+ * Builds a string from an array of lines.
+ *
+ * @param {string[]} lines Array of strings to join into a single string.
+ * @returns {string} The built string with lines separated by a newline character.
+ */
 export default function textBuilder(lines: string[]): string {
   return lines.join("\n");
 }
