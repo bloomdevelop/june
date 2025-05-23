a1a2196c903ee1c16055939f8820a5bfc7099656
Bloom Perez <bloomdevelop@proton.me>
2025-05-23 13:53:12 +0000

    feat: Implement database

---
+++ src/db/schema.ts
@@ -0,0 +1,10 @@
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { ulid } from "ulid";
+
+export const messages = sqliteTable("messages", {
+  id: text("id").primaryKey().$defaultFn(() => {
+    return ulid();
+  }),
+  userId: integer("user_id"),
+  content: text("content"),
+});
